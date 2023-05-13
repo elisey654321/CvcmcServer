96c525b94b480305479d261fbbbd6eaf8cc8dc1e
elisey654321 <elisey654321@mail.ru>
2023-05-13 21:55:07 +0300

    Create project

---
+++ src/main/java/ru/cvcmc/CvcmcServer/CvcmcServerApplication.java
@@ -0,0 +1,13 @@
+package ru.cvcmc.CvcmcServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvcmcServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvcmcServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/cvcmc/CvcmcServer/CvcmcServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.cvcmc.CvcmcServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvcmcServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
